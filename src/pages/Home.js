ffa6efc26436e27d2da300f7eb9bf321a68c81cb
mwaustephen <stephenmwau41@gmail.com>
2023-07-22 13:51:57 +0300

    added the new products slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroSlider from "../components/sliders/HeroSlider";
 import FeaturedSlider from "../components/sliders/FeaturedSlider";
+import NewProducts from "../components/sliders/NewProducts";
 import SectionsHead from "../components/common/SectionsHead";
 import TopProducts from "../components/product/TopProducts";
 import Services from "../components/common/Services";
@@ -14,9 +15,13 @@ const Home = () => {
 
       <section id="featured" className="section">
         <div className="container">
-          <SectionsHead heading="Featured Products" />
-          <FeaturedSlider />
+          <SectionsHead heading="New Products" />
+          <NewProducts />
         </div>
+      </section>
+
+      <section id="banner" className="section">
+        <div className="container"></div>
       </section>
 
       <section id="products" className="section">
